84bec52adfa2fba5ef43a93fe54c25f732d94a2c
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2022-10-27 22:53:43 -0500

    configure app init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SigninPage from "./pages/auth/SigninPage";
+import Err404Page from "./pages/err/Err404Page";
+import HomePage from "./pages/home/HomePage";
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        {/* Auth */}
+        <Route path="/signin" element={<SigninPage />} />
+
+
+
+        <Route path="*" element={<Err404Page />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/home/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function HomePage() {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+
+export default HomePage
