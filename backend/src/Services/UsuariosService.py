3e8c0785e87edc31536e67900238812fa2a8b9c4
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2022-10-28 23:17:15 -0500

    add get user method

--- backend/src/Services/UsuariosService.py
+++ backend/src/Services/UsuariosService.py
@@ -9,6 +9,7 @@ from Entities.UsuarioEntity import UsuarioEntity
 from Entities.PersonaEntity import PersonaEntity
 from Utils.DTOs.RegistroDTO import RegistroDTO
 from Utils.DTOs.UsuarioDTO import UsuarioDTO
+from Utils.DTOs.PersonaDTO import PersonaDTO
 from Utils.Helpers.StringsHelper import StringsHelper
 from Utils.Strategies.JWTStrategy import write_jwt
 
@@ -48,3 +49,39 @@ class UsuariosService:
         finally:
             usuarioDTO = None
 
+    def get_usuario(self, usuario_id: int) -> UsuarioEntity | None:
+        try:
+            usuarioDTO = UsuarioDTO()
+            personaDTO = PersonaDTO()
+            
+            usuario = self.usuarioRepository.join_persona(usuario_id=usuario_id)
+
+            if usuario_id != None:
+
+                personaDTO.persona_id = usuario.persona_id.persona_id
+                personaDTO.telefono = usuario.persona_id.telefono
+                personaDTO.apaterno = usuario.persona_id.apaterno
+                personaDTO.amaterno = usuario.persona_id.amaterno
+                personaDTO.nombre = usuario.persona_id.nombre
+                personaDTO.genero = usuario.persona_id.genero
+
+                usuarioDTO.usuario_id = usuario.usuario_id
+                usuarioDTO.persona = personaDTO
+                usuarioDTO.rol = usuario.rol
+                usuarioDTO.email = usuario.email
+                usuarioDTO.token = usuario.token
+                usuarioDTO.isActive = usuario.isActive
+                usuarioDTO.createdAt = str(usuario.createdAt)
+                
+                return usuarioDTO
+
+            else:
+                return None
+            
+ 
+        except Exception as e:
+            logging.ERROR(traceback.format_exc())
+            return e
+
+        finally:
+            usuarioDTO = None
