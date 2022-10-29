8244c41d00ce4040459ef77e007b87dfddaa78fe
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2022-10-29 18:11:08 -0500

    fetch to get users

--- frontend/src/hooks/useFetchUsuarios.tsx
+++ frontend/src/hooks/useFetchUsuarios.tsx
@@ -1,19 +1,30 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { IResponse } from '../utils/interfaces/backend/IResponse';
 import IUsuario from '../utils/interfaces/IUsuario';
+import useAsyncStorage from './useAsynStorage';
 
 export interface IUsuariosResponse extends IResponse {
     resultado: string | IUsuario
 }
 
 function useFetchUsuarios() {
-
+    const { getItem } = useAsyncStorage();
+    
+    const jwt = () => getItem().then((data) => data?.jwt);
+    
     const url = (process.env.REACT_APP_BE_BASE_URI as string) +
-        (process.env.REACT_APP_BE_BASE_PATH as string) + '/usuarios'
+    (process.env.REACT_APP_BE_BASE_PATH as string) + '/usuarios'
 
     const getUsuarios = useCallback(async (): Promise<IUsuario[]> => {
-        const response = await fetch(url)
+
+        const response = await fetch(url + '/', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + jwt
+            }
+        })
         const data: IUsuariosResponse = await response.json()
+        console.log(data)
         if (Array.isArray(data.resultado)) {
             return data.resultado as IUsuario[]
         }
