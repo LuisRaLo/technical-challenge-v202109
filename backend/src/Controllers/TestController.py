13b33afc164c173300b211fa6067f72111542282
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2022-10-28 23:16:10 -0500

    add test controller

---
+++ backend/src/Controllers/TestController.py
@@ -0,0 +1,34 @@
+import logging
+from re import T
+import traceback
+import json
+from Services.UsuariosService import UsuariosService
+from Utils.Strategies.JWTStrategy import validate_jwt
+from flask import Blueprint, request, jsonify
+from Utils.DTOs.RegistroDTO import RegistroDTO
+from Services.AuthService import AuthService
+from Utils.Helpers.StringsHelper import StringsHelper
+
+testController = Blueprint("testController", __name__)
+
+
+@testController.route("/isValidateJwt", methods=["GET"])
+def get_usuarios():
+    try:
+        token = request.headers.get("Authorization")
+
+        if token and validate_jwt(token=token, output=False) == True:
+            return validate_jwt(token=token, output=True)
+        else:
+            return validate_jwt(token=token, output=False)
+    except Exception as e:
+        logging.exception(traceback.format_exc())
+        response = jsonify(
+            {
+                "folio": StringsHelper.generate_folio(),
+                "mensaje": "Operación fallida",
+                "resultado": "Revisa tus datos",
+            }
+        )
+        response.status_code = 500
+        return response
