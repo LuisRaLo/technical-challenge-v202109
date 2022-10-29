251499274707f0ebf3b8ba57f0dddff7191d7f54
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2022-10-28 23:16:26 -0500

    add jwt validation

--- backend/src/Controllers/UsuariosController.py
+++ backend/src/Controllers/UsuariosController.py
@@ -3,6 +3,7 @@ import traceback
 import json
 from Services.UsuariosService import UsuariosService
 from Utils.Strategies.JWTStrategy import validate_jwt
+from Utils.Helpers.ValidateHelper import ValidateHelper
 from flask import Blueprint, request, jsonify
 from Utils.DTOs.RegistroDTO import RegistroDTO
 from Services.AuthService import AuthService
@@ -16,9 +17,9 @@ usuariosService = UsuariosService()
 def get_usuarios():
     try:
         token = request.headers.get("Authorization")
-                        
-        if token and validate_jwt(token=token.split(" ")[1], output=False) == True:
-            
+
+        if token and validate_jwt(token=token, output=False) == True:
+
             proccess = usuariosService.get_usuarios()
 
             if proccess != None:
@@ -47,6 +48,68 @@ def get_usuarios():
             return response
         else:
             return validate_jwt(token=token, output=True)
+    except Exception as e:
+        logging.exception(traceback.format_exc())
+        response = jsonify(
+            {
+                "folio": StringsHelper.generate_folio(),
+                "mensaje": "Operación fallida",
+                "resultado": "Revisa tus datos",
+            }
+        )
+        response.status_code = 500
+        return response
+
+
+@usuariosController.route("/<int:usuario_id>", methods=["GET"])
+def get_usuario_by_id(usuario_id: int):
+    try:
+        token = request.headers.get("Authorization")
+
+        if ValidateHelper.validate_request(
+            request={"usuario_id": usuario_id}, required=["usuario_id"]
+        ):
+
+            if token and validate_jwt(token=token, output=False) == True:
+                proccess = usuariosService.get_usuario(usuario_id)
+                if proccess != None:
+                    return (
+                        jsonify(
+                            {
+                                "folio": StringsHelper.generate_folio(),
+                                "mensaje": "Operación exitosa",
+                                "resultado": proccess.toJSON(),
+                            }
+                        ),
+                        200,
+                    )
+                else:
+                    response = (
+                        jsonify(
+                            {
+                                "folio": StringsHelper.generate_folio(),
+                                "mensaje": "Operación fallida",
+                                "resultado": "Revisa tus datos",
+                            }
+                        ),
+                        400,
+                    )
+
+                return response
+            else:
+                return validate_jwt(token=token, output=True)
+        else:
+            return (
+                jsonify(
+                    {
+                        "folio": StringsHelper.generate_folio(),
+                        "mensaje": "Operación fallida",
+                        "resultado": "Revisa tus datos",
+                    }
+                ),
+                400,
+            )
+
     except Exception as e:
         logging.exception(traceback.format_exc())
         response = jsonify(
